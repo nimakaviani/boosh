0bbb00144313c73c0d7a44f714a609a8ee093b8a
Nima Kaviani <nkavian@us.ibm.com>
2017-04-18 21:11:14 -0700

    fix boosh to retrieve more than 100 resources
    - DescribeStackResources only retrives first 100 resources
    - ListStackResources paginates the list of resoruces if there is more
    than 100 resource
    - Change boosh resource command to use ListStackResources

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "boosh"
 	app.Usage = "BOOSH Outer Outer Shell"
-	app.Version = "0.0.1"
+	app.Version = "0.0.1.1"
 
 	app.Commands = []cli.Command{
 		{
